dcc73fab88f8757aa2fbf71c8515a4c63b551dd5
Hossam Amer <hossam.amer@multicom-mac2.uwaterloo.ca>
2020-11-03 14:04:27 -0500

    fixed a typo

--- argfcompile.sh
+++ argfcompile.sh
@@ -4,6 +4,6 @@
 #/usr/local/bin/gcc-6 -lc++ -O2 -w -lboost_timer -lstdc++ -o sparse -I/Users/hossam.amer/7aS7aS_Works/work/my_Tools/eigen -fopenmp $1
 
 # If im2col encoding
-#/usr/local/bin/gcc-6 -lc++ -O3 -march=native -ffast-math  -msse4 -w -lboost_timer -lstdc++ -o sparse -I/Users/hossam.amer/7aS7aS_Works/work/my_Tools/eigen -I/Users/hoss    am.amer/7aS7aS_Works/work/my_Tools/ATLAS/include -fopenmp -lcblas -llapack $1
+#/usr/local/bin/gcc-6 -lc++ -O3 -march=native -ffast-math  -msse4 -w -lboost_timer -lstdc++ -o sparse -I/Users/hossam.amer/7aS7aS_Works/work/my_Tools/eigen -I/Users/hossam.amer/7aS7aS_Works/work/my_Tools/ATLAS/include -fopenmp -lcblas -llapack $1
 
 echo "/usr/local/bin/gcc-6 -lc++ -O3 -march=native -ffast-math  -msse4 -w -lboost_timer -lstdc++ -o sparse -I/Users/hossam.amer/7aS7aS_Works/work/my_Tools/eigen -fopenmp $1"
